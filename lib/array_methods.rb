cdd2727cac54411afce3521404fb7790cc3b3d21
john L <johnleavell@gmail.com>
2019-10-20 03:38:56 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,8 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
   element1 = "wow"
-  array.include?("shoe")
+  element2 = "Tardis"
+  array.include?("Tardis")
 
 
 end
